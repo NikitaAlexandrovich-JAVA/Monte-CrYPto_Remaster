09ab877008193668b8c4df93fbd51fa43a51c77a
NikitaAlexandrovich-JAVA <pukaievich00@mail.ru>
2025-02-18 00:25:26 +0300

    Создан класс Messages

---
+++ src/Messages.java
@@ -0,0 +1,26 @@
+public class Messages {
+
+    public final static String START_MESSAGE = "\nВас приветствует программа по кодированию и декодированию текста \"Монте-КриПто\"\n";
+
+    public final static String MY_SKILL_MESSAGE = """
+            МОИ ВОЗМОЖНОСТИ:   /  Команды управления:
+            
+            1. Закодировать   -> 1
+            2. Раскодировать  -> 2
+            3. Взломать       -> 3
+            4. Завершение     -> exit 
+            
+            ПРЕДУПРЕЖДЕНИЕ: -  функция №3 не совсем законна!!! )
+            """;
+
+    public final static String END_MESSAGE = "СООБЩЕНИЕ: - программа завершена";
+
+    public final static String INPUT_KEY_MESSAGE = "Введите ключ %s:";
+
+    public final static String ERROR_NOT_COMMAND_MESSAGE = "ОШИБКА : <<нет такой команды>>";
+
+
+}
+
+
+
