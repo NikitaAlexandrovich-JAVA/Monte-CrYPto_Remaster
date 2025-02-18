503bd84bc5b1b9779ed80cdc4d19d0ff64b80832
NikitaAlexandrovich-JAVA <pukaievich00@mail.ru>
2025-02-18 20:28:17 +0300

    Модифицирован класс Alphabet

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -27,10 +27,26 @@ public class Alphabet {
         alphabetArrayList.add(';');
         alphabetArrayList.add('!');
 
+
         return alphabetArrayList;
     }
 
-    public ArrayList<Character> getAlphabetArrayList() {
-        return alphabetArrayList;
+//    public ArrayList<Character> getAlphabetArrayList() {
+//        return alphabetArrayList;
+//    }
+
+    public int size() {
+        return alphabetArrayList.size();
+    }
+
+    public char get(int index){
+        return alphabetArrayList.get(index);
+    }
+
+    public boolean contains (char symbol){
+        return alphabetArrayList.contains(symbol);
+    }
+    public int indexOf(char symbol){
+        return alphabetArrayList.indexOf(symbol);
     }
 }
