12a515f6299fda22e6aa9982a44d667c3b165624
NikitaAlexandrovich-JAVA <pukaievich00@mail.ru>
2025-02-18 20:17:40 +0300

    Добавлен класс Encrypted

---
+++ src/Encrypted.java
@@ -0,0 +1,7 @@
+public class Encrypted extends EnDeCrypted {
+
+    @Override
+    public void crypted(int key, String pathRead, String pathSave) {
+        super.crypted(key, pathRead, pathSave);
+    }
+}
